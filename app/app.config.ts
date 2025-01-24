4cf520eadd2997853118630055f4dae75692aae2
TribeWeb <rich@tribeweb.co.uk>
2025-01-24 12:21:02 +0000

    1 change by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'green',
-      neutral: 'slate'
+      primary: 'purple',
+      neutral: 'slate',
+      gray: 'zinc'
     },
     icons: {
       chevronDown: 'i-lucide-chevron-right'
