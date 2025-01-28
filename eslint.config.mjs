530859492e6933331e731511bf6e9dde277949d2
rich-tribeweb <rich@tribeweb.co.uk>
2025-01-28 23:03:02 +0000

    fix: eslint stopped working

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  {
+export default withNuxt({
     rules: {
       '@stylistic/max-len': ['error', { code: 160 }],
       '@stylistic/js/eol-last': ['error', 'always']
     }
-  }
-)
+  })
+//   .overrideRules({
+//   'vue/max-attributes-per-line': ['error', { singleline: 5 }]
+// })
