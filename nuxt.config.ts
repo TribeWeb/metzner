ee6c3a59288fb9b281e53a0d45a0fa0b60f9102c
rich-tribeweb <rich@tribeweb.co.uk>
2025-02-04 18:58:05 +0000

    chore: enable devtools timeline

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,11 @@ export default defineNuxtConfig({
   ],
 
   devtools: {
-    enabled: true
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
   },
 
   css: ['~/assets/css/main.css'],
