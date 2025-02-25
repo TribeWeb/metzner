1e2caf722dd4cb4b4f6bd37f28f02459fabefe6f
rich-tribeweb <rich@tribeweb.co.uk>
2025-02-25 15:07:45 +0000

    feat: add contact form component

---
+++ app/components/content/contactForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+useScript({
+  src: 'https://userresources.prospect365.com/forms/QXZhbiBNZWR2ZWRldkFudvUt0fa+nPox7g%252Fhhu4C+wDua6PEgA==/3/form.js'
+})
+</script>
+
+<template>
+  <div id="prospect-form-3">
+    <div class="prospect-form-loading" />
+  </div>
+  <!-- <script defer src="https://userresources.prospect365.com/forms/QXZhbiBNZWR2ZWRldkFudvUt0fa+nPox7g%252Fhhu4C+wDua6PEgA==/3/form.js" /> -->
+</template>
+
+<style scoped>
+.prospect-form-loading {
+    width: "50px";
+    height:"50px";
+    margin:"0 auto";
+    background:"url(https://userresources.prospect365.com/forms/spinner.svg) center center no-repeat";
+    background-size:"contain"
+  }
+</style>
