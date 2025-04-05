bb245ae20472c02b8746076927c7ecf2f5d8ae69
rich-tribeweb <rich@tribeweb.co.uk>
2025-04-05 15:20:48 +0100

    fix: mend deleted props not updating

--- app/middleware/persistQuery.global.ts
+++ app/middleware/persistQuery.global.ts
@@ -3,6 +3,7 @@ import { defineNuxtRouteMiddleware } from 'nuxt/app'
 export default defineNuxtRouteMiddleware((to, from) => {
   if (import.meta.server) return
 
+  if (to.path === from.path) return // Don't navigate if paths are the same
   const mergedQuery = { ...from.query, ...to.query } // Merge previous and new query parameters
 
   // Prevent infinite redirects by only navigating if queries are different
