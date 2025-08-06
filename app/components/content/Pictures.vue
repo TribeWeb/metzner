bad3be9998d5dc49812cc73ce746d26348d76e78
rich-tribeweb <rich@tribeweb.co.uk>
2025-08-06 15:31:15 +0100

    feat: add prose components

---
+++ app/components/content/Pictures.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  orientation?: 'horizontal' | 'vertical'
+}
+
+const { orientation = 'horizontal' } = defineProps<Props>()
+</script>
+
+<template>
+  <div
+    class="flex items-center justify-between"
+    :class="orientation === 'vertical' ? 'flex-col space-y-4' : 'flex-row space-x-4'"
+  >
+    <slot />
+  </div>
+</template>
