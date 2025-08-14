0aaa86597ea051937ff4e54e2403add4b3151d82
rich-tribeweb <rich@tribeweb.co.uk>
2025-08-14 14:59:57 +0100

    feat: add schemaOrg module & test

--- app/pages/machines/[slug].vue
+++ app/pages/machines/[slug].vue
@@ -32,6 +32,138 @@ useSeoMeta({
 defineOgImageComponent('Docs')
 
 const headline = computed(() => findPageHeadline(navigation?.value, route.path))
+
+useSchemaOrg([
+  defineProduct({
+    name: `Metzner ${page.value.title}`,
+    description: page.value.description,
+    disambiguatingDescription: page.value.features?.description,
+    image: `/machines/${page.value.path.split('/').pop()}.webp`, // TODO
+    category: 'Universal Cutting Machine',
+    sameAs: 'https://www.metzner.com/product-solutions/profiles-tubes/universal-modular-cutting-machines/metzner-variocut/', // Add to Google Sheet
+    url: route.fullPath,
+    brand: {
+      '@type': 'Brand',
+      'name': seo?.siteName,
+      'url': 'https://metzner.co.uk/about'
+    },
+    manufacturer: {
+      '@type': 'Organization',
+      'name': 'Metzner Maschinenbau GmbH',
+      'url': 'https://www.metzner.com/'
+    },
+    mpn: page.value.title,
+    countryOfOrigin: 'DE',
+    weight: '65 kg', // Add to Google Sheet
+    hasMeasurement: [
+      {
+        '@type': 'QuantitativeValue',
+        'name': 'Material Width Max',
+        'value': page.value.cutWidth,
+        'unitText': 'mm',
+        'unitCode': 'MMT',
+        'description': 'Maximum width of material that can be processed'
+      },
+      {
+        '@type': 'QuantitativeValue',
+        'name': 'Material Height Max',
+        'value': page.value.cutHeight,
+        'unitText': 'mm',
+        'unitCode': 'MMT',
+        'description': 'Maximum height of material that can be processed'
+      },
+      {
+        '@type': 'QuantitativeValue',
+        'name': 'Material Diameter Max',
+        'value': page.value.cutDiameter,
+        'unitText': 'mm',
+        'unitCode': 'MMT',
+        'description': 'Maximum diameter of material that can be processed'
+      },
+
+      {
+        '@type': 'QuantitativeValue',
+        'name': 'Length Interval',
+        'value': 0.1, // Add to Google Sheet
+        'unitText': 'mm',
+        'unitCode': 'MMT',
+        'description': 'Minimum length interval for cutting'
+      },
+      {
+        '@type': 'QuantitativeValue',
+        'name': 'Feeding Speed Max',
+        'value': 90, // Add to Google Sheet
+        'unitText': 'm/min',
+        'description': 'Maximum feeding speed'
+      },
+      {
+        '@type': 'QuantitativeValue',
+        'name': 'Cutting Performance',
+        'value': page.value.cutRate,
+        'unitText': 'cuts/min',
+        'unitCode': 'CPM',
+        'description': 'Maximum cutting performance'
+      }
+    ],
+    additionalProperty: [
+      {
+        '@type': 'PropertyValue',
+        'name': 'Available Cutting Technologies',
+        'value': 'Die cutting, Shear cutting, Draw cutting', // Add to Google Sheet??
+        'description': 'Types of cutting technologies supported'
+      },
+      {
+        '@type': 'PropertyValue',
+        'name': 'Cutting Accuracy',
+        'value': page.value.accuracy,
+        'description': 'Positional and repeatable cutting accuracy'
+      },
+      {
+        '@type': 'PropertyValue',
+        'name': 'Dimensions (L×W×H)',
+        'value': '765×515×460 mm',
+        'description': 'Length × Width × Height'
+      },
+      {
+        '@type': 'PropertyValue',
+        'name': 'Electrical Input',
+        'value': '230 Vac; 50/60 Hz; 10 A',
+        'description': 'Power supply requirements'
+      },
+      {
+        '@type': 'PropertyValue',
+        'name': 'Compressed Air Input',
+        'value': '6 bar | 87 PSI',
+        'description': 'Required compressed air connection'
+      },
+      {
+        '@type': 'PropertyValue',
+        'name': 'Consumption kVA | bar',
+        'value': 'P: 0.4 kVA | approx. 100 l/min.',
+        'description': 'Power and air consumption'
+      },
+      {
+        '@type': 'PropertyValue',
+        'name': 'Noise (idle)',
+        'value': '< 75dB (A)',
+        'description': 'Noise level during idle operation'
+      }
+    ],
+    offers: {
+      '@type': 'AggregateOffer',
+      'url': route.fullPath,
+      'priceCurrency': 'GBP',
+      'lowPrice': '5000', // Add to Google Sheet
+      'highPrice': '10000', // Add to Google Sheet
+      'itemCondition': 'https://schema.org/NewCondition',
+      'seller': {
+        '@type': 'Organization',
+        'name': 'CP Power & Automation',
+        'url': 'https://cppowerautomation.com'
+      }
+    }
+  })
+])
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineLocalBusiness } from 'nuxt-schema-org/schema'
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/ui-pro',
@@ -9,7 +11,8 @@ export default defineNuxtConfig({
     'nuxt-og-image',
     '@nuxthub/core',
     '@nuxt/scripts',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    'nuxt-schema-org'
   ],
 
   devtools: {
@@ -85,5 +88,109 @@ export default defineNuxtConfig({
 
   ogImage: {
     zeroRuntime: true
+  },
+
+  schemaOrg: {
+    identity: defineLocalBusiness({
+      // '@type': '...', // Choose from https://schema.org/LocalBusiness#subtypes
+
+      // Basic Information (Required)
+      name: 'The Coastal Kitchen',
+      description: 'Farm-to-table restaurant specializing in sustainable seafood and seasonal ingredients',
+      url: 'https://thecoastalkitchen.com',
+
+      // Location (Required)
+      address: {
+        streetAddress: '742 Oceanview Boulevard, Suite 100',
+        addressLocality: 'Santa Cruz',
+        addressRegion: 'CA',
+        postalCode: '95060',
+        addressCountry: 'US'
+      },
+
+      // Precise Geographic Location, if applicable
+      geo: {
+        '@type': 'GeoCoordinates',
+        'latitude': '36.9741',
+        'longitude': '-122.0308'
+      },
+
+      // Contact Information, if applicable
+      telephone: '+1-831-555-0123',
+      email: 'hello@thecoastalkitchen.com',
+
+      // Hours of Operation, if applicable
+      openingHoursSpecification: [
+        {
+          dayOfWeek: ['Monday', 'Tuesday', 'Wednesday', 'Thursday'],
+          opens: '11:30:00',
+          closes: '22:00:00'
+        },
+        {
+          dayOfWeek: ['Friday', 'Saturday'],
+          opens: '11:30:00',
+          closes: '23:00:00'
+        },
+        {
+          dayOfWeek: 'Sunday',
+          opens: '10:00:00', // Sunday Brunch
+          closes: '21:00:00'
+        }
+      ],
+
+      // Business Details, if applicable
+      priceRange: '$$$', // $, $$, $$$, or $$$$
+      servesCuisine: [
+        'Seafood',
+        'California',
+        'Farm-to-table'
+      ],
+
+      // Menu (for restaurants)
+      menu: 'https://thecoastalkitchen.com/menu',
+
+      // Images, if applicable
+      image: [
+        'https://thecoastalkitchen.com/images/storefront.jpg',
+        'https://thecoastalkitchen.com/images/interior.jpg',
+        'https://thecoastalkitchen.com/images/food.jpg'
+      ],
+      logo: '/logo.png',
+
+      // Payment Options, if applicable
+      paymentAccepted: [
+        'Cash',
+        'Credit Card',
+        'Cryptocurrency'
+      ],
+      currenciesAccepted: 'USD',
+
+      // Additional Business Details, if applicable
+      isAccessibleForDisabled: true,
+      amenityFeature: [
+        {
+          '@type': 'LocationFeatureSpecification',
+          'name': 'Parking',
+          'value': true
+        },
+        {
+          '@type': 'LocationFeatureSpecification',
+          'name': 'Wheelchair Accessible',
+          'value': true
+        },
+        {
+          '@type': 'LocationFeatureSpecification',
+          'name': 'Outdoor Seating',
+          'value': true
+        }
+      ],
+
+      // Social Links, if applicable
+      sameAs: [
+        'https://www.facebook.com/coastalkitchen',
+        'https://instagram.com/thecoastalkitchen',
+        'https://twitter.com/coastalkitchen'
+      ]
+    })
   }
 })
