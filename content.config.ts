5a9788d87d552b0d6cc927092ec4fa7fa85c2bf2
rich-tribeweb <rich@tribeweb.co.uk>
2025-10-06 21:22:07 +0100

    fix: remove asSitemapCollection as it's not working

--- content.config.ts
+++ content.config.ts
@@ -1,18 +1,15 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, property } from '@nuxt/content'
 import { z } from 'zod'
 import { asSitemapCollection } from '@nuxtjs/sitemap/content'
 import { schemas } from './app/utils/schemas'
 
 export default defineContentConfig({
   collections: {
-    home: defineCollection(
-      asSitemapCollection({
+    home: defineCollection({
         type: 'page',
         source: 'index.md'
-      })
-    ),
-    about: defineCollection(
-      asSitemapCollection({
+      }),
+    about: defineCollection({
         type: 'page',
         source: [{
           include: 'about/*.md',
@@ -20,55 +17,42 @@ export default defineContentConfig({
         },
         { include: '1.about.md' }],
         schema: schemas.about
-      })
-    ),
-    machinesLanding: defineCollection(
-      asSitemapCollection({
+     }),
+    machinesLanding: defineCollection({
         type: 'page',
         source: 'machines.md'
-      })
-    ),
-    machines: defineCollection(
-      asSitemapCollection({
+      }),
+    machines: defineCollection({
         type: 'page',
         source: 'machines/*.md',
         schema: schemas.machines
-      })
-    ),
-    sparesLanding: defineCollection(
-      asSitemapCollection({
+      }),
+    sparesLanding: defineCollection({
         type: 'page',
         source: 'spares.md'
-      })
-    ),
-    peripheralsLanding: defineCollection(
-      asSitemapCollection({
+      }),
+    peripheralsLanding: defineCollection({
         type: 'page',
         source: 'peripherals.md'
-      })
-    ),
-    peripherals: defineCollection(
-      asSitemapCollection({
+      }),
+    peripherals: defineCollection({
         type: 'page',
         source: 'peripherals/*.md'
-      })
-    ),
-    latest: defineCollection(
-      asSitemapCollection({
+      }),
+    latest: defineCollection({
         type: 'page',
         source: 'latest.md'
-      })
-    ),
+      }),
     posts: defineCollection({
       source: 'latest/**/*',
       type: 'page',
       schema: z.object({
-        image: z.object({ src: z.string().nonempty().editor({ input: 'media' }) }),
+        image: z.object({ src: property(z.string()).editor({ input: 'media' }) }),
         authors: z.array(
           z.object({
             name: z.string().nonempty(),
             to: z.string().nonempty(),
-            avatar: z.object({ src: z.string().nonempty().editor({ input: 'media' }) })
+            avatar: z.object({ src: property(z.string()).editor({ input: 'media' }) })
           })
         ),
         date: z.date(),
