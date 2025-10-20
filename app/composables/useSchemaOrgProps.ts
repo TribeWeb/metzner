2e0b38951f8136389501d88f09786c5bf0483271
rich-tribeweb <rich@tribeweb.co.uk>
2025-10-20 22:40:00 +0100

    feat: make schemaOrg values dynamic

---
+++ app/composables/useSchemaOrgProps.ts
@@ -0,0 +1,22 @@
+import type { MachinesCollectionItem } from '@nuxt/content'
+
+export function useSchemaOrgProps(machine: MachinesCollectionItem) {
+  const schemaOrgProps = useAppConfig().schemaOrg
+
+  return schemaOrgProps.map((prop) => {
+    const value = machine[prop.value as keyof MachinesCollectionItem]
+    if (value === undefined || value === null || value === '') {
+      return null
+    }
+
+    return {
+      '@type': prop['@type'] || undefined,
+      'name': prop.name || undefined,
+      'description': prop?.description || undefined,
+      'value': `${value}${prop.unitText}`,
+      'unitText': prop.unitText || undefined,
+      'unitCode': prop.unitCode || undefined,
+      'schema': prop.schema || undefined
+    }
+  }).filter(prop => prop !== null)
+}
