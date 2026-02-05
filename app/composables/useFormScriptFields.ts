aa4bb5c03abee1c1a3e950e2f3ae2a7cff72d4d6
rich-tribeweb <rich@tribeweb.co.uk>
2026-02-05 17:47:33 +0000

    fix: when parsing the string remove reliance on minified variable names

--- app/composables/useFormScriptFields.ts
+++ app/composables/useFormScriptFields.ts
@@ -29,13 +29,13 @@ export function extractFormFields($: cheerio.CheerioAPI) {
 
 export async function useFormScriptFields(formId: string, prospectFormOrgId: string) {
   const { data } = await useFetch<string>(`https://userresources.prospect365.com/forms/${prospectFormOrgId}/${formId}/form.js`)
-
-  const htmlString = computed(() => data.value?.toString().split('a.innerHTML=')?.[1]?.split(';a.className')[0])
+  const htmlString = computed(() => data.value?.toString().split('.innerHTML=')?.[1]?.split('.className')[0])
   const html = computed(() => htmlString.value?.substring(1, htmlString.value.length - 1).replace(/\\"/g, '"').replace(/\\n/g, '').replace(/\\t/g, ' '))
 
   const formFields = computed(() => {
     const $ = cheerio.load(html.value || '')
     return extractFormFields($)
   })
+
   return formFields
 }
