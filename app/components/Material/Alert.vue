bc654e27806f9d1c838162342fd33e144062de6c
rich-tribeweb <rich@tribeweb.co.uk>
2026-02-17 22:44:57 +0000

    feat: machine page widget

--- app/components/Material/Alert.vue
+++ app/components/Material/Alert.vue
@@ -1,123 +1,115 @@
 <script setup lang="ts">
-import { materials, machines } from '#imports'
-import { z } from 'zod'
-
-const machineSchema = machines.pick({ cutDiameter: true, cutWidth: true, cutHeight: true })
-const materialSchema = materials.pick({ stiffness: true, shape: true, reinforced: true, core: true })
-
-const schema = z.object({ ...machineSchema.shape, ...materialSchema.shape })
-type Schema = z.output<typeof schema>
-
-const state = reactive<Partial<Schema>>({})
+import type { Reactive } from 'vue'
 
 const route = useRoute()
-onMounted(() => {
-  const parsedRoute = schema.parse(route.query) as Partial<Schema>
-  Object.assign(state, parsedRoute)
-})
+const state = inject('state') as Reactive<Partial<Schema>>
 
 const { data: page } = await useAsyncData(`${route.path}-title`, () => {
   return queryCollection('machines').path(route.path).select('title').first()
 })
 
+await useAsyncData(`machinesData`, () => {
+  return queryCollection('machines')
+    .select('modelId', 'machineName', 'machineId', 'cutDiameter', 'cutWidth', 'cutHeight')
+    .all()
+})
+
+const copy = computed(() => ({
+  success: {
+    title: `The ${page.value?.title || 'machine'} will cut the material you have specified`,
+    description: 'The machine will cut the material you have specified with the dimensions and properties you have chosen.',
+    icon: 'i-lucide-circle-check-big',
+    actions: [
+      {
+        label: 'View specifications',
+        trailingIcon: `${showDisplay.value ? 'i-lucide-arrow-big-up' : 'i-lucide-arrow-big-down'}`,
+        onClick: openDisplay
+      }
+    ]
+  },
+  warning: {
+    title: `The ${page.value?.title || 'machine'} will not cut the material you have specified`,
+    description: 'View a list of compatible machines or please adjust your specifications and try again.',
+    icon: 'i-lucide-triangle-alert',
+    actions: [
+      {
+        label: 'View compatible machines',
+        trailingIcon: 'i-tabler-blade-filled',
+        onClick: openDisplay
+      },
+      {
+        label: 'Edit specifications',
+        trailingIcon: 'i-lucide-square-pen',
+        onClick: openDisplayAndPicker
+      }
+    ]
+  },
+  info: {
+    title: 'Machine choosing guide',
+    description: 'Specify the material you need to cut, and we\'ll identify the most suitable cutting machines from our range...',
+    icon: 'i-lucide-info',
+    actions: [
+      {
+        label: 'Start now',
+        trailingIcon: 'i-lucide-square-pen',
+        onClick: openDisplayAndPicker
+      }
+    ]
+  }
+})
+)
+
 const noMachineMatch = computed(() => {
-  return state.cutDiameter ? state.cutDiameter > 18 : false
+  return state.diameter ? state.diameter > 19 : false
 })
 
 const status = computed(() => {
   if (!state.shape || !state.core || !state.reinforced) {
-    return 'incomplete'
+    return 'info'
   }
   if (noMachineMatch.value) {
-    return 'error'
+    return 'warning'
   }
   return 'success'
 })
 
 const showDisplay = ref(false)
-function openSpecifications() {
+function openDisplay() {
   showDisplay.value = !showDisplay.value
 }
+const showPicker = ref(false)
 
-const success = ref({
-  title: `The ${page.value?.title || 'machine'} will cut the material you have specified`,
-  color: 'success' as const,
-  variant: 'subtle' as const,
-  description: 'The machine will cut the material you have specified with the dimensions and properties you have chosen.',
-  icon: 'i-lucide-check',
-  actions: [
-    {
-      label: 'View specifications',
-      color: 'success' as const,
+function openDisplayAndPicker() {
+  showDisplay.value = true
+  showPicker.value = true
+}
+
+const alertActions = computed(() => {
+  return copy.value[status.value].actions
+    .filter(action => action?.label && action?.onClick)
+    .map(action => ({
+      label: action.label,
+      color: `${status.value}` as const,
       variant: 'subtle' as const,
       size: 'md' as const,
-      trailingIcon: 'i-lucide-square-pen',
-      onClick: openSpecifications
-    }
-  ],
-  ui: {
-    icon: 'size-11',
-    title: 'text-lg'
-  }
-})
-
-const incomplete = ref({
-  title: 'Machine choosing guide',
-  color: 'info' as const,
-  variant: 'subtle' as const,
-  description: 'Specify the material you need to cut, and we\'ll identify the most suitable cutting machines from our range..',
-  icon: 'i-lucide-alert-triangle',
-  actions: [
-    {
-      label: 'Start now',
-      color: 'info' as const,
-      variant: 'subtle' as const,
-      size: 'md' as const,
-      trailingIcon: 'i-lucide-square-play'
-    }
-  ],
-  ui: {
-    icon: 'size-11',
-    title: 'text-lg'
-  }
-})
-
-const error = ref({
-  title: `The ${page.value?.title || 'machine'} will not cut the material you have specified`,
-  color: 'warning' as const,
-  variant: 'subtle' as const,
-  description: 'View a list of compatible machines or please adjust your specifications and try again.',
-  icon: 'i-lucide-x',
-  actions: [
-    {
-      label: 'Compatible machines',
-      color: 'warning' as const,
-      variant: 'subtle' as const,
-      size: 'md' as const,
-      trailingIcon: 'i-tabler-blade-filled'
-    },
-    {
-      label: 'Edit specifications',
-      color: 'warning' as const,
-      variant: 'outline' as const,
-      size: 'md' as const,
-      trailingIcon: 'i-lucide-square-play',
-      onClick: openSpecifications
-    }
-  ],
-  ui: {
-    icon: 'size-11',
-    title: 'text-lg'
-  }
+      trailingIcon: action.trailingIcon,
+      onClick: action.onClick
+    }))
 })
 </script>
 
 <template>
-  <UAlert v-if="status === 'success'" v-bind="success" />
-  <UAlert v-else-if="status === 'error'" v-bind="error" />
-  <UAlert v-else-if="status === 'incomplete'" v-bind="incomplete" />
+  <UAlert
+    v-bind="copy[status]"
+    variant="subtle"
+    :color="status"
+    :ui="{ icon: 'size-11', title: 'text-lg' }"
+    :actions="alertActions"
+  />
   <Transition name="slide-fade">
-    <MaterialDisplay v-if="showDisplay" />
+    <div v-if="showDisplay">
+      <MaterialDisplay :show-picker="showPicker" />
+    </div>
   </Transition>
 </template>
 
