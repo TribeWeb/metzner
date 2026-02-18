c0089e6dfcb04818cb5a52add94150073184e584
rich-tribeweb <rich@tribeweb.co.uk>
2026-02-18 14:03:15 +0000

    chore: deactivate sitemap plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
-    '@nuxtjs/sitemap',
+    // '@nuxtjs/sitemap',
     '@nuxt/content',
     '@nuxt/image',
     'nuxt-og-image',
